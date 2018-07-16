cc89e160cfee4cb259f55508aa41706803aaa813
maria <mkulakova923@gmail.com>
2018-07-16 23:11:28 +0300

    35.03 https://www.youtube.com/watch?v=93p3LxR9xfM

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -9,30 +9,52 @@ class PostForm extends Component {
     };
 
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div>
         <h1>Add Post</h1>
-        <div>
-          <label>Title:</label>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title:</label>
+            <br />
+            <input type="text" name="title" onChange={this.onChange}
+              value={this.state.title}/>
+          </div>
           <br />
-          <input type="text" name="title" onChange={this.onChange}
-            value={this.state.title}/>
-        </div>
-        <br />
-        <div>
-          <label>Body:</label>
-          <br />
-          <textarea type="text" name="body" onChange={this.onChange}
-            value={this.state.body} />
-        </div>
-        <button type="submit">Submit</button>
+          <div>
+            <label>Body:</label>
+            <br />
+            <textarea type="text" name="body" onChange={this.onChange}
+              value={this.state.body} />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
